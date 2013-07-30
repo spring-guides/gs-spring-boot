2f5af0f0088989ea5ccdd718a050fdd0c7814bf7
Greg Turnquist <gturnquist@gopivotal.com>
2013-07-30 15:41:09 -0400

    Initial commit of Spring Boot guide

---
+++ complete/src/main/java/hello/Application.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableWebMvc
+@ComponentScan
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ complete/src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	public @ResponseBody String index() {
+		return "Greetings from Spring Boot!";
+	}
+	
+}
