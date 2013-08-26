433d87794095522d6b47dd43a3badf22e225a69d
Phillip Webb <pwebb@gopivotal.com>
2013-08-26 09:51:41 -0700

    Remove superfluous @EnableWebMvc annotation

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -10,11 +10,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableWebMvc
 @ComponentScan
 public class Application {
     
