e21783d04403cc98db2ec6fc0f9c487eb340321d
pavangit <pavan.rb@gmail.com>
2015-12-07 14:40:06 -0500

    Updated HelloController
    Testing Commit

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings PAVAN !!! from Spring Boot!";
     }
     
 }
