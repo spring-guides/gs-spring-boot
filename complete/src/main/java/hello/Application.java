528f05fa5516c7b22c237ec771ebc6eaef7772f4
Wasim Thabraze <wasim@thabraze.me>
2017-07-24 16:16:04 +0530

    test commit

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -19,7 +19,7 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            System.out.println("Let's inspect the beans-----test provided by Spring Boot:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
