526de39fd0f0703f340504214692ae6431485a57
Greg Turnquist <gturnquist@pivotal.io>
2018-03-01 09:54:52 -0600

    Upgrade to Spring Boot 2.0.0.RELEASE

--- complete/src/test/java/hello/HelloControllerIT.java
+++ complete/src/test/java/hello/HelloControllerIT.java
@@ -1,7 +1,7 @@
 package hello;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 import java.net.URL;
 
@@ -9,9 +9,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
