fa6c3785ad97f19c67606a6f0d7d70dd51780812
Kylie Liang <kyliel@microsoft.com>
2018-09-19 11:29:09 +0800

    Add imports

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
