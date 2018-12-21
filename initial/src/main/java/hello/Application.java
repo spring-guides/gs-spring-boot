bf2d811c5b33133f7aa98b4b6e4778d156de259f
Taras Didyk <tarasdidyk94@gmail.com>
2018-12-21 09:38:38 +0000

    New

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -6,11 +6,7 @@ package hello;
 public class Application {
 
     public static void main(String[] args) {
-        baseSleep();
-        open();
-        baseSleep();
-        open();
-        baseSleep();
+        
         open();
         baseSleep();
         open();
