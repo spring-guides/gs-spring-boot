291ec54247e3a77a11134d290bf6af6b830e7c13
testpodgit7 <testdtestdt@gmail.com>
2019-03-19 13:24:25 +0000

    test

---
+++ complete/src/OutPrint.java
@@ -0,0 +1,17 @@
+public class CallingMethodsInSameClass
+{
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwoы() {
+		printOne();
+		printOne();
+	}
+}
