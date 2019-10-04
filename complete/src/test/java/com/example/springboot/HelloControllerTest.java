4f2277459dfa4adfb92edbd18a5c49ce82bc8085
Jay Bryant <jbryant@pivotal.io>
2019-10-04 12:33:11 -0500

    Use the Spring Initializr
    I rewrote the document and edited the code (mostly changing package names) to use the Spring Initializr
    
    I also upgraded the build files to Spring Boot 2.2.0, which necessitated changing the tests to use JUnit 5 and replacing the gradle wrappers.

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -1,20 +1,18 @@
-package hello;
+package com.example.springboot;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
