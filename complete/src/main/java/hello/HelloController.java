119af9b6e5a5099770fd0fe6ad43ca955b129e0e
Michael Katan <michaelkatan@gmail.com>
2019-10-28 15:23:04 +0000

    testCommit

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,14 +1,24 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
+
+    @RequestMapping("/RandomNumber")
+    public int RandomNumber()
+    {
+        Random rand = new Random();
+        int x = rand.nextInt(100);
+        return x;
+    }
 }
