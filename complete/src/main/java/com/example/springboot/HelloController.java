d66ada2cc31d86113c289ec9b467cba792c53575
shubh agrawal <shubh.coding@gmail.com>
2020-10-03 12:40:49 +0530

    changed the annotation and the API call

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
+	@PostMapping("/start")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
