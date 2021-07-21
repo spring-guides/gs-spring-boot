d8ad7523a14a7ccc0972980da768b1c7b1a4c131
Brian Clozel <bclozel@vmware.com>
2021-07-21 12:16:57 +0200

    Update guide for Spring Boot 2.5.x
    The `@RequestMapping` annotation is replaced by `@GetMapping` since HTTP
    verbs variants are preferred.
    
    The actuator info endpoint is not exposed by default anymore.
    
    The local server port is automatically configured in the base URL for
    `TestRestTemplate`.

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -1,12 +1,12 @@
 package com.example.springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
