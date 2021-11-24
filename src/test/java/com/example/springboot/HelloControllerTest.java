e22241053f7fd6c3305afa68c1cfad62ac0f44c7
Pierre Louis Conte <Plouis.conte@gmail.com>
2021-11-24 10:14:20 +0100

    Update test

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -23,8 +23,7 @@ public class HelloControllerTest {
 	@Test
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
+				.andExpect(!status().isOk())
 				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
-		org.junit.Assert.assertTrue(false);
 	}
 }
