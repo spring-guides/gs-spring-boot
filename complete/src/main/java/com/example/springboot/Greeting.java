4291754bd38e13c3467eb8d743c6d903eef86a0d
alexsey <prohin.alexsey@gmail.com>
2023-08-12 19:34:23 +0300

    inizial

---
+++ complete/src/main/java/com/example/springboot/Greeting.java
@@ -0,0 +1,4 @@
+package com.example.springboot;
+
+public record Greeting(long id, String content) {
+}
---
+++ complete/src/main/java/com/example/springboot/GreetingController.java
@@ -0,0 +1,20 @@
+package com.example.springboot;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.example.springboot.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
