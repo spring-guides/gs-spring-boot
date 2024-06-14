7cfb3519f253a73a15934aac4b074d4b6ffbe109
Anonymous <anonymous@anonymous>
2024-06-13 21:50:58 -0700

    test

--- complete/src/main/java/com/example/springboot/Application.java
+++ complete/src/main/java/com/example/springboot/Application.java
@@ -14,7 +14,6 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
