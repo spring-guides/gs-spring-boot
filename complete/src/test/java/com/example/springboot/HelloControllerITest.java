f7021a776f0b4dcd1a50f95f07c7d79754966f11
domingo1021 <domingo.backend@gmail.com>
2024-07-06 16:05:17 +0800

    refactor: make HelloControllerITest match maven wildcard pattern

--- complete/src/test/java/com/example/springboot/HelloControllerITest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerITest.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HelloControllerIT {
+public class HelloControllerITest {
 
 	@Autowired
 	private TestRestTemplate template;
