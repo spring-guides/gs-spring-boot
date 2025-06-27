075cb42e540edee861fa1f1336480d38ccd27917
RmZBrN <briniramzi@hotmail.com>
2025-06-27 16:28:06 +0200

    Agent: Update unit test for new greeting message

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -23,7 +23,7 @@ public class HelloControllerTest {
 	@Test
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+			.andExpect(status().isOk())
+			.andExpect(content().string(equalTo("Hello my Fellow Agents!")));
 	}
 }
