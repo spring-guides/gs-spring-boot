59bacd4f64cd6ae189cdeb958b04e913c5d4b5ac
RmZBrN <briniramzi@hotmail.com>
2025-07-01 15:14:19 +0200

    Define FlightJpaEntity with JPA and Lombok annotations

--- src/main/java/com/planespotter/infrastructure/persistence/entities/FlightJpaEntity.java
+++ src/main/java/com/planespotter/infrastructure/persistence/entities/FlightJpaEntity.java
@@ -1,1 +1,46 @@
 package com.planespotter.infrastructure.persistence.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "flights")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FlightJpaEntity {
+
+    @Id
+    private String id;
+
+    private String flightNumber;
+    private String airline;
+    private String originAirportIata;
+    private String destinationAirportIata;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime scheduledDepartureUtc;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime actualDepartureUtc;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime scheduledArrivalUtc;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime actualArrivalUtc;
+
+    private String aircraftIcao;
+    private String status;
+    private String departureGate;
+    private String departureRunway;
+    private String arrivalGate;
+    private String arrivalRunway;
+}
