7dab76fc40b00d04b5d122b7a0b93b7bb4ce6aa0
RmZBrN <briniramzi@hotmail.com>
2025-07-01 13:12:15 +0200

    Implement Location entity with Lombok annotations

--- src/main/java/com/planespotter/domain/entities/Location.java
+++ src/main/java/com/planespotter/domain/entities/Location.java
@@ -1,1 +1,13 @@
 package com.planespotter.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+    private String type;
+    private String identifier;
+}
