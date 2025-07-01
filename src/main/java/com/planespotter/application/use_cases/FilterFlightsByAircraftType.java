67c97a94f98cf6ed36f05a6ad33484fbc50bf98d
RmZBrN <briniramzi@hotmail.com>
2025-07-01 13:19:27 +0200

    Create FilterFlightsByAircraftType.java with package declaration

--- src/main/java/com/planespotter/application/use_cases/FilterFlightsByAircraftType.java
+++ src/main/java/com/planespotter/application/use_cases/FilterFlightsByAircraftType.java
@@ -1,17 +1,1 @@
 package com.planespotter.application.use_cases;
-
-import com.planespotter.domain.entities.Flight;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Service
-public class FilterFlightsByAircraftType {
-
-    public List<Flight> execute(List<Flight> flights, String aircraftIcaoCode) {
-        return flights.stream()
-                .filter(flight -> flight.getAircraftIcao().equalsIgnoreCase(aircraftIcaoCode))
-                .collect(Collectors.toList());
-    }
-}
