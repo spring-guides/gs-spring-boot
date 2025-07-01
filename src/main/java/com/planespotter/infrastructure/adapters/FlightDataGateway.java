aba458b3723c2e7c1b0ad1c55e27a6394735d500
RmZBrN <briniramzi@hotmail.com>
2025-07-01 13:19:32 +0200

    Create FlightDataGateway.java with package declaration

--- src/main/java/com/planespotter/infrastructure/adapters/FlightDataGateway.java
+++ src/main/java/com/planespotter/infrastructure/adapters/FlightDataGateway.java
@@ -1,8 +1,1 @@
 package com.planespotter.infrastructure.adapters;
-
-import com.planespotter.domain.entities.Flight;
-import java.util.List;
-
-public interface FlightDataGateway {
-    List<Flight> getFlights(String airportIataCode);
-}
