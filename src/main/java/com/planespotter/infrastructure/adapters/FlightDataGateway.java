3f60a06406900dfb330655a29156daf5c29e9506
RmZBrN <briniramzi@hotmail.com>
2025-07-01 13:12:52 +0200

    Define FlightDataGateway interface

--- src/main/java/com/planespotter/infrastructure/adapters/FlightDataGateway.java
+++ src/main/java/com/planespotter/infrastructure/adapters/FlightDataGateway.java
@@ -1,1 +1,8 @@
 package com.planespotter.infrastructure.adapters;
+
+import com.planespotter.domain.entities.Flight;
+import java.util.List;
+
+public interface FlightDataGateway {
+    List<Flight> getFlights(String airportIataCode);
+}
