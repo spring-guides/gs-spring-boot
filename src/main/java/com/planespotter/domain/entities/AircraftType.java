b9ac8128f835b966902787175d70d457fde775c0
RmZBrN <briniramzi@hotmail.com>
2025-07-01 14:32:51 +0200

    Implement AircraftType entity with Lombok annotations

--- src/main/java/com/planespotter/domain/entities/AircraftType.java
+++ src/main/java/com/planespotter/domain/entities/AircraftType.java
@@ -1,1 +1,15 @@
 package com.planespotter.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AircraftType {
+    private String icaoCode;
+    private String manufacturer;
+    private String model;
+    private String description;
+}
