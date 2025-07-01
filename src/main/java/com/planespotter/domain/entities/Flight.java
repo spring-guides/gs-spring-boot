13a63e4fa82aade9711cf9b9193116437172010c
RmZBrN <briniramzi@hotmail.com>
2025-07-01 13:12:03 +0200

    Implement Flight entity with Lombok annotations

--- src/main/java/com/planespotter/domain/entities/Flight.java
+++ src/main/java/com/planespotter/domain/entities/Flight.java
@@ -1,1 +1,27 @@
 package com.planespotter.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Flight {
+    private String id;
+    private String flightNumber;
+    private String airline;
+    private String originAirportIata;
+    private String destinationAirportIata;
+    private LocalDateTime scheduledDepartureUtc;
+    private LocalDateTime actualDepartureUtc;
+    private LocalDateTime scheduledArrivalUtc;
+    private LocalDateTime actualArrivalUtc;
+    private String aircraftIcao;
+    private String status;
+    private String departureGate;
+    private String departureRunway;
+    private String arrivalGate;
+    private String arrivalRunway;
+}
