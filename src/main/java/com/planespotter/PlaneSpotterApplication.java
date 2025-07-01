d5023f9ea15c7bc730d927da89272f8db0ab026f
RmZBrN <briniramzi@hotmail.com>
2025-07-01 14:33:32 +0200

    Create PlaneSpotterApplication main class

--- src/main/java/com/planespotter/PlaneSpotterApplication.java
+++ src/main/java/com/planespotter/PlaneSpotterApplication.java
@@ -1,1 +1,12 @@
 package com.planespotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneSpotterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaneSpotterApplication.class, args);
+    }
+}
