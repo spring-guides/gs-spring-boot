ddccf60772ebedeeb528f95660cb6a9daf75357c
RmZBrN <briniramzi@hotmail.com>
2025-07-01 15:14:33 +0200

    Define FlightJpaRepository interface extending JpaRepository

--- src/main/java/com/planespotter/infrastructure/persistence/repositories/FlightJpaRepository.java
+++ src/main/java/com/planespotter/infrastructure/persistence/repositories/FlightJpaRepository.java
@@ -1,1 +1,9 @@
 package com.planespotter.infrastructure.persistence.repositories;
+
+import com.planespotter.infrastructure.persistence.entities.FlightJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FlightJpaRepository extends JpaRepository<FlightJpaEntity, String> {
+}
