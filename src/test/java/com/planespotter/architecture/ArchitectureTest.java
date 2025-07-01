101b8b5ae2ebd291973ed2553d6309c2dbdd30e9
RmZBrN <briniramzi@hotmail.com>
2025-07-01 15:15:27 +0200

    Implement ArchUnit tests for architectural validation

--- src/test/java/com/planespotter/architecture/ArchitectureTest.java
+++ src/test/java/com/planespotter/architecture/ArchitectureTest.java
@@ -1,1 +1,23 @@
 package com.planespotter.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchTests;
+import com.tngtech.archunit.junit.ArchUnitRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ArchUnitRunner.class)
+@AnalyzeClasses(packages = "com.planespotter")
+public class ArchitectureTest {
+
+    @ArchTest
+    public static final ArchTests domainRules = ArchTests.in(DomainArchitectureRules.class);
+
+    @ArchTest
+    public static final ArchTests applicationRules = ArchTests.in(ApplicationArchitectureRules.class);
+
+    @ArchTest
+    public static final ArchTests infrastructureRules = ArchTests.in(InfrastructureArchitectureRules.class);
+}
+
+// Separate classes for each layer's rules can be defined similarly
