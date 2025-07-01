e783885ebb552ae2852cfa7dc2cc3476e5f1fa5b
RmZBrN <briniramzi@hotmail.com>
2025-07-01 13:19:21 +0200

    Create Airport.java with package declaration

--- src/main/java/com/planespotter/domain/entities/Airport.java
+++ src/main/java/com/planespotter/domain/entities/Airport.java
@@ -1,15 +1,1 @@
 package com.planespotter.domain.entities;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Airport {
-    private String iataCode;
-    private String name;
-    private String city;
-    private String country;
-}
