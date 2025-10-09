abeb3e3289407c0fbcac453c3a8a0010f0801347
Danil.Pavlov <danil.pavlov@jetbrains.com>
2025-10-09 17:57:29 +0200

    Upgrade to Spring Boot 3.5.6
    Signed-off-by: Danil Pavlov <danil.pavlov@jetbrains.com>

--- complete-kotlin/build.gradle.kts
+++ complete-kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.3"
+    id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
