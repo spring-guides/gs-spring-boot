5b5b237a0af8eb1e14b5065307448441b9aca73d
Rafael Machado <rafaelzm2000@gmail.com>
2026-01-17 10:14:28 -0500

    Fix package declaration formatting in AuthController
    Signed-off-by: Rafael Machado <rafaelzm2000@gmail.com>

--- complete/src/main/java/com/example/springboot/AuthController.java
+++ complete/src/main/java/com/example/springboot/AuthController.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package com.example.springboot; 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
